41623d55589f2507afe63174d2cc50704af45b94
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2023-12-08 17:42:04 +0700

    update from Hao-2

--- CSE305_BusApp_Group5/src/entity/App_Manager.java
+++ CSE305_BusApp_Group5/src/entity/App_Manager.java
@@ -1,51 +1,26 @@
 package entity;
+
 import java.util.ArrayList;
 
 public class App_Manager {
-    public ArrayList<User> listofPassenger;
-    public ArrayList<Routes> listofRoutesAvailable;
-    private int App_Manager_ID;
-    private String accesss_level;
-    private String reporting_line;
-    
 
-public App_Manager(int app_Manager_ID, String accesss_level, String reporting_line,ArrayList<User> listofPassenger,ArrayList<Routes> listofRoutesAvailable) {
+    private String App_Manager_ID;
+    private String name;
+    private String dob;
 
-	this.App_Manager_ID = app_Manager_ID;
-	this.accesss_level = accesss_level;
-	this.reporting_line = reporting_line;
-	this.listofPassenger=listofPassenger;
-    this.listofRoutesAvailable=listofRoutesAvailable;
-}
-    // public void addPassenger(User user) {
-    //     listofPassenger.add(user);
-    // }
-    // public void listPassenger() {
-    //     for (int i=0; i<listofPassenger.size(); i++) {
-    //         System.out.println(listofPassenger.get(i)+"\n");
-    //     }
-    // }
-    // chưa đủ đi kiện hiện thực
-    public void addRoutes(Routes routes) {
-        listofRoutesAvailable.add(routes);
-    }
-    public void listRoutes() {
-        for (int i=0; i<listofRoutesAvailable.size(); i++) {
-            System.out.println(listofRoutesAvailable.get(i)+"\n");
-        }
+    public App_Manager(String app_Manager_ID, String name, String dob) {
+
+        this.App_Manager_ID = app_Manager_ID;
+        this.name = name;
+        this.dob = dob;
     }
 
-    public void UpdateRoutes(int route_ID,double distance, double duration, String route_Status) {
-        listofRoutesAvailable.get(route_ID).UpdateRoute(route_ID, distance, duration, route_Status);
+    public String getName() {
+        return name;
     }
 
-    
-
-    public void DeleteRoutes(int id) {
-        listofRoutesAvailable.remove(id);
+    public String getDob() {
+        return dob;
     }
 
-    public void DeletePassenger(int id) {
-        listofPassenger.remove(id);
-    }
 }
