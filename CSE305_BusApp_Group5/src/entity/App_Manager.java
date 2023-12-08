68c55c74c775fb9b34669b050b6f20d6e688f5d6
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2023-12-08 11:03:35 +0700

    up from Hao Nguyen

--- CSE305_BusApp_Group5/src/entity/App_Manager.java
+++ CSE305_BusApp_Group5/src/entity/App_Manager.java
@@ -1,45 +1,45 @@
 package entity;
+
 import java.util.ArrayList;
 
 public class App_Manager {
+
     public ArrayList<User> listofPassenger;
     public ArrayList<Routes> listofRoutesAvailable;
     private int App_Manager_ID;
-    private String accesss_level;
-    private String reporting_line;
+    private String name;
+    private String dob;
+
+    public App_Manager(int app_Manager_ID, String name, String dob, ArrayList<User> listofPassenger, ArrayList<Routes> listofRoutesAvailable) {
+
+        this.App_Manager_ID = app_Manager_ID;
+        this.name = name;
+        this.dob = dob;
+        this.listofPassenger = listofPassenger;
+        this.listofRoutesAvailable = listofRoutesAvailable;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
     
-
-public App_Manager(int app_Manager_ID, String accesss_level, String reporting_line,ArrayList<User> listofPassenger,ArrayList<Routes> listofRoutesAvailable) {
-
-	this.App_Manager_ID = app_Manager_ID;
-	this.accesss_level = accesss_level;
-	this.reporting_line = reporting_line;
-	this.listofPassenger=listofPassenger;
-    this.listofRoutesAvailable=listofRoutesAvailable;
-}
-    // public void addPassenger(User user) {
-    //     listofPassenger.add(user);
-    // }
-    // public void listPassenger() {
-    //     for (int i=0; i<listofPassenger.size(); i++) {
-    //         System.out.println(listofPassenger.get(i)+"\n");
-    //     }
-    // }
-    // chưa đủ đi kiện hiện thực
     public void addRoutes(Routes routes) {
         listofRoutesAvailable.add(routes);
     }
+
     public void listRoutes() {
-        for (int i=0; i<listofRoutesAvailable.size(); i++) {
-            System.out.println(listofRoutesAvailable.get(i)+"\n");
+        for (int i = 0; i < listofRoutesAvailable.size(); i++) {
+            System.out.println(listofRoutesAvailable.get(i) + "\n");
         }
     }
 
-    public void UpdateRoutes(int route_ID,double distance, double duration, String route_Status) {
+    public void UpdateRoutes(int route_ID, double distance, double duration, String route_Status) {
         listofRoutesAvailable.get(route_ID).UpdateRoute(route_ID, distance, duration, route_Status);
     }
-
-    
 
     public void DeleteRoutes(int id) {
         listofRoutesAvailable.remove(id);
---
+++ CSE305_BusApp_Group5/src/entity/sqlInfomation.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Asus
+ */
+public class sqlInfomation {
+
+    private String url;
+    private String acc;
+    private String pass;
+
+    public sqlInfomation() {
+        this.url = "jdbc:mysql://localhost:3306/bus_app";
+        this.acc = "root";
+        this.pass = "Halazara979489";
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getAcc() {
+        return acc;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+}
